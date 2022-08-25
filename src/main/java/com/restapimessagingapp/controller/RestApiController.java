9331b9c6063a3a464e1e248ca5c9ed4a6402c518
bans04 <bansodednyanadeo@gmail.com>
2022-08-25 15:20:13 +0530

    Added:-> Use GET Request Method and pass name as uery parameter

--- src/main/java/com/restapimessagingapp/controller/RestApiController.java
+++ src/main/java/com/restapimessagingapp/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.restapimessagingapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class RestApiController {
     @GetMapping("/hello")
     public String message(){
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping(value = "/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from Bridgelabz ";
     }
 }
